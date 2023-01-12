1561645727b45de71b488ff1bf95f7cd9ea261b5
Orazio Conte <orazioconte@iMac-di-Orazio-2.local>
2023-01-12 19:28:17 +0100

    File base per la gestione dei file in python

---
+++ copyfile.py
@@ -0,0 +1,53 @@
+# import shutil
+# import os
+
+# src = "/Users/orazioconte/Desktop/django-script/manage_file/partenza"
+# dst = "/Users/orazioconte/Desktop/django-script/manage_file/destinazione"
+
+
+# shutil.copy(src=src + '/sole.jpg', dst=dst + '/sole.jpg')
+
+# =================================================================
+
+# src_path = r"/Users/orazioconte/Desktop/django-script/manage_file/partenza/static/base.html"
+# dst_path = r"/Users/orazioconte/Desktop/django-script/manage_file/destinazione/"
+# shutil.copy(src_path, dst_path)
+# print('Copied')
+
+# =================================================================
+
+# import os
+# import shutil
+# from shutil import SameFileError
+
+# src_folder = r"/Users/orazioconte/Desktop/django-script/manage_file/partenza/static/"
+# dst_folder = r"/Users/orazioconte/Desktop/django-script/manage_file/destinazione/static/"
+
+# # printing the contents of the destination folder
+# print("Destination folder before copying::", os.listdir(dst_folder))
+
+# # file names
+# src_file = src_folder + "base.html"
+# dst_file = dst_folder + "base.html"
+
+# try:
+#     # copy file
+#     shutil.copyfile(src_file, dst_file)
+#     # destination folder after copying
+#     print("Destination after copying", os.listdir(dst_folder))
+# except SameFileError:
+#     print("We are trying to copy the same File")
+# except IsADirectoryError:
+#     print("The destination is a directory")
+
+
+# =================================================================
+
+# ! OOOK FUNZIONA E COPIA LA DIRECTORY PIù LE SOTTO DIRECTORY CONTENUTE
+# ! Questo è il master base principale
+
+import shutil
+
+source_dir = r"/Users/orazioconte/Desktop/django-script/manage_file/partenza/static/"
+destination_dir = r"/Users/orazioconte/Desktop/django-script/manage_file/destinazione/static/"
+shutil.copytree(source_dir, destination_dir)
